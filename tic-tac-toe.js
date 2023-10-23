ecff1dba6c4310cd9b7f67140831329b45733b7b
Michael Benjamin <mbenji99@gmail.com>
2023-10-22 21:06:55 -0500

    Exercise 4 Done

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -32,6 +32,50 @@ document.addEventListener("DOMContentLoaded", () => {
         square.classList.remove("hover");
       });
     });
-  });
   
+    let gameOver = false;
+    let winner = null;
+  
+    squares.forEach((square) => {
+      square.addEventListener("click", () => {
+        if (!gameOver && !square.classList.contains("X") && !square.classList.contains("O")) {
+          square.classList.remove("hover");
+          square.classList.add(currentPlayer);
+          square.textContent = currentPlayer;
+  
+          if (checkWinner(currentPlayer)) {
+            gameOver = true;
+            winner = currentPlayer;
+            document.getElementById("status").classList.add("you-won");
+            document.getElementById("status").textContent = `Congratulations! ${currentPlayer} is the Winner!`;
+          } else if (checkTie()) {
+            gameOver = true;
+            document.getElementById("status").textContent = "It's a Tie!";
+          } else {
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
+          }
+        }
+      });
+    });
+  
+    function checkWinner(player) {
+      const lines = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6],            // Diagonals
+      ];
+  
+      for (const line of lines) {
+        const [a, b, c] = line;
+        if (squares[a].textContent === player && squares[b].textContent === player && squares[c].textContent === player) {
+          return true;
+        }
+      }
+      return false;
+    }
+  
+    function checkTie() {
+      return [...squares].every((square) => square.classList.contains("X") || square.classList.contains("O"));
+    }
+  });
   
