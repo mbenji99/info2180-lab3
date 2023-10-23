1acb4e2f66bf157bff8571547eb5bbc7fdb1c304
Michael Benjamin <mbenji99@gmail.com>
2023-10-22 19:56:36 -0500

    Exercise 2 Done

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const squares = document.querySelectorAll('#board div');
+    let currentPlayer = 'X';
 
     squares.forEach((square) => {
         square.classList.add('square');
+        square.addEventListener('click', function () {
+            if (!square.textContent) { // Check if the square is empty
+                square.textContent = currentPlayer; // Set X or O
+                square.classList.add(currentPlayer); // Add X or O class
+                currentPlayer = currentPlayer === 'X' ? 'O' : 'X'; // Toggle player
+            }
+        });
     });
 });
